ba2eee8fce63ed3d2f154a31e8cf1fac7d3b6b1a
prikhodko_an <prihodkoan@gmail.com>
2019-03-19 21:40:17 +0300

    Add base project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Disk from './scenes/Disk';
+import Login from './scenes/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/disk" component={Disk} />
+        <Redirect to="/login" />
+      </Switch>
     );
   }
 }
---
+++ src/scenes/Disk/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Disk = () => <div>Disk</div>;
+
+export default Disk;
