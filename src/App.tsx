9d0803211d0fb764c28d6d4b5b3cd99216a46806
prikhodko_an <prihodkoan@gmail.com>
2019-03-19 21:50:46 +0300

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+import Header from './components/Header';
 import Disk from './scenes/Disk';
 import Login from './scenes/Login';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/disk" component={Disk} />
-        <Redirect to="/login" />
-      </Switch>
+      <>
+        <Header />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/disk" component={Disk} />
+          <Redirect to="/login" />
+        </Switch>
+      </>
     );
   }
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Header = () => (
+  <header className={styles.container}>ReactYandexDisk</header>
+);
+
+export default Header;
