e3d02d290c178e83105347b27a8a2b76db9e30bf
prikhodko_an <prihodkoan@gmail.com>
2019-03-24 18:25:55 +0300

    Add user name  and logout button into the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
+import { selectAuth } from '../../services/auth/selectors';
+import { IAuthState } from '../../services/auth/models';
+
+import logo from './public/logo.svg';
 import styles from './index.module.scss';
 
-const Header = () => (
+interface IStateProps {
+  auth: IAuthState;
+}
+
+interface IProps extends IStateProps {}
+
+const Header = ({ auth: { user, isAuthenticated } }: IProps) => (
   <header className={styles.container}>
-    <Container>ReactYandexDisk</Container>
+    <Container className="d-flex justify-content-between align-items-center">
+      <Link to="/disk">
+        <img className={styles.logo} src={logo} alt="React Yandex Disk" />
+      </Link>
+      {isAuthenticated && (
+        <div className={styles.userData}>
+          <div className={styles.userName}>
+            {user.real_name ? user.real_name : user.display_name}
+          </div>
+          <Button>Выход</Button>
+        </div>
+      )}
+    </Container>
   </header>
 );
 
-export default Header;
+const mapStateToProps = createStructuredSelector<any, IStateProps>({
+  auth: selectAuth,
+});
+
+export default connect<IStateProps, {}>(
+  mapStateToProps,
+  {}
+)(Header);
